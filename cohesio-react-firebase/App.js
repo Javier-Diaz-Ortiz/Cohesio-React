537f989351a8ac9f0599f8ec52a6064263f53e6b
javdiaort <javdiaort@alum.us.es>
2025-01-15 12:41:53 +0100

    deleted createUserScreen and UserLists

--- cohesio-react-firebase/App.js
+++ cohesio-react-firebase/App.js
@@ -3,9 +3,6 @@ import {NavigationContainer} from '@react-navigation/native'
 import {createStackNavigator}from '@react-navigation/stack'
 
 const Stack =createStackNavigator()
-
-import UsersList from './screens/UsersList';
-import CreateUserScreen from './screens/CreateUserScreen';
 import UserDetailScreen from './screens/UserDetailScreen';
 import CohesioMainScreen from './screens/CohesioMainScreen';
 import ProjectsScreen from './screens/ProjectsScreen';
@@ -19,8 +16,6 @@ function MyStack(){
   return (
     <Stack.Navigator>
       <Stack.Screen name="CohesioMainScreen" component={CohesioMainScreen} />  
-      <Stack.Screen name="UsersList" component={UsersList} options={{title: 'Who are you?'}}/> 
-      <Stack.Screen name="CreateUserScreen"  component={CreateUserScreen} options={{title: 'Create a new user'}}/>
       <Stack.Screen name="UserDetailScreen"  component={UserDetailScreen} options={{title: 'User Detail'}} />
       <Stack.Screen name="ProjectsScreen"  component={ProjectsScreen} options={{title: 'Projects Screen'}} />
       <Stack.Screen name="CreateReview"  component={CreateReview} options={{title: 'Create Review'}} />
