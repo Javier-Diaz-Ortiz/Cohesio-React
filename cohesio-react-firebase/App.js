3460347871c06aba5ec164fd439778b232fb4117
javdiaort <javdiaort@alum.us.es>
2025-01-20 15:31:09 +0100

    project finished correctly

--- cohesio-react-firebase/App.js
+++ cohesio-react-firebase/App.js
@@ -3,26 +3,22 @@ import {NavigationContainer} from '@react-navigation/native'
 import {createStackNavigator}from '@react-navigation/stack'
 
 const Stack =createStackNavigator()
-import UserDetailScreen from './screens/UserDetailScreen';
 import CohesioMainScreen from './screens/CohesioMainScreen';
 import ProjectsScreen from './screens/ProjectsScreen';
 import CreateReview from './screens/CreateReview';
 import ReviewScreen from './screens/ReviewScreen';
 import LoginScreen from './screens/LoginScreen';
 import RegisterScreen from './screens/RegisterScreen';
-import UserHomeScreen from './screens/UserHomeScreen';
 
 function MyStack(){
   return (
     <Stack.Navigator>
       <Stack.Screen name="CohesioMainScreen" component={CohesioMainScreen} />  
-      <Stack.Screen name="UserDetailScreen"  component={UserDetailScreen} options={{title: 'User Detail'}} />
       <Stack.Screen name="ProjectsScreen"  component={ProjectsScreen} options={{title: 'Projects Screen'}} />
       <Stack.Screen name="CreateReview"  component={CreateReview} options={{title: 'Create Review'}} />
       <Stack.Screen name="ReviewScreen"  component={ReviewScreen} options={{title: 'Review Screen'}} />
       <Stack.Screen name="LoginScreen"  component={LoginScreen} options={{title: 'Login'}} />
       <Stack.Screen name="RegisterScreen"  component={RegisterScreen} options={{title: 'Register'}} />
-      <Stack.Screen name="UserHomeScreen"  component={UserHomeScreen} options={{title: 'User Home Screen'}} />
       {/* TODO : put all screens here  */}
 
       </Stack.Navigator>
